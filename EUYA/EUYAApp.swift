d15e7a954d1061000f16e43734529b26946a02f4
Mercen <mercen@mercen.net>
2023-01-11 15:35:07 +0900

    Initial Commit

---
+++ EUYA/EUYAApp.swift
@@ -0,0 +1,17 @@
+//
+//  EUYAApp.swift
+//  EUYA
+//
+//  Created by Mercen on 2023/01/11.
+//
+
+import SwiftUI
+
+@main
+struct EUYAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
